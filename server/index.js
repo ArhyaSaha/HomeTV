32cd965ccfef062b848d13ca8ace638ada5c72c8
ArhyaSaha <arhyapratimsaha28@gmail.com>
2025-06-27 21:54:37 +0530

    update CORS

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
-  credentials: true
+  origin: '*',
+  credentials: false
 }));
 app.use(express.json());
 
